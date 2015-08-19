55ee6af209612ea70e0fd17715d3e30420447a4d
Rajiv Nair <rajivrnair@gmail.com>
2015-08-19 16:05:52 +0530

    Added request and error logging

--- lib/app.rb
+++ lib/app.rb
@@ -8,6 +8,19 @@ configure {
 }
 
 class Brahma < Sinatra::Base
+  configure do
+    enable :logging
+    access_log = File.new("#{settings.root}/../log/access-#{settings.environment}.log", 'a+')
+    access_log.sync = true
+    use Rack::CommonLogger, access_log
+  end
+
+  error_log = ::File.new("#{settings.root}/../log/error-#{settings.environment}.log","a+")
+  error_log.sync = true
+  before {
+    env["rack.errors"] = error_log
+  }
+
   get '/ping' do
     return 'Brahma Lives!!'
   end
