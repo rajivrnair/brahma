380e2038072b1c7fdb2f0364cabce01210fc4dda
Rajiv Nair <rajivrnair@gmail.com>
2015-08-28 01:22:23 +0530

    set deploy to directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/rajivrnair/brahma.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/rnair/deployments'
+set :deploy_to, '/home/deployer/'
 
 # Default value for :scm is :git
 # set :scm, :git
