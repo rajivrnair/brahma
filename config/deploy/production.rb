81c0f860709548187405c988aa46336742bf24da
Rajiv Nair <rajivrnair@gmail.com>
2015-08-28 01:38:41 +0530

    use public key to authenticate deployer

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,11 +41,11 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
+ set :ssh_options, {
 #    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+   auth_methods: %w(publickey)
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
